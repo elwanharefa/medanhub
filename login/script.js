5b833d10d08a8572ae100d7d316d16e52290ff63
Elwan Harefa <elwanharefa@gmail.com>
2021-06-10 09:48:59 +0700

    tampilan login

---
+++ login/script.js
@@ -0,0 +1,12 @@
+function showpassword() {
+    var x = document.getElementById("input-password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function login(){
+    console.log("berhasil")
+}
